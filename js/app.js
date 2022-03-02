1c38a007c34600749e3b08c4b294f8aaabced31b
arifvtti <arif.vtti@gmail.com>
2022-03-02 08:57:21 +0600

    api call part done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
+// api key
 const API_KEY = `eb4a6ccd3ac741458f67a50a3dd2bd24`;
-const loadTem = () => {
+
+//load temperature when click search button
+const loadTemp = () => {
     const inputText = document.getElementById("input-field");
     const city = inputText.value;
     console.log(city);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
         .then(res => res.json())
-        .then(data => displayTem(data));
+        .then(data => displayTemp(data));
 }
 
-const displayTem = tem => {
-    console.log(tem);
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemp = temp => {
+    console.log(temp);
+    // set innertext by using id
+    setInnerText("city-name", temp.name);
+    setInnerText("temp", temp.main.temp);
+    setInnerText("condition", temp.weather[0].main);
+
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById("imgIcon");
+    imgIcon.setAttribute("src", url);
+
 }
